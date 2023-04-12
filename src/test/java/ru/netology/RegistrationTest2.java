1d04ee9b83972f45d537e4204f0e959dc381006f
Ivan Vasilev <tor419@yandex.ru>
2023-04-12 21:27:08 +0700

    chore: add tests commit

--- src/test/java/ru/netology/RegistrationTest2.java
+++ src/test/java/ru/netology/RegistrationTest2.java
@@ -14,29 +14,30 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class RegistrationTest {
+public class RegistrationTest2 {
 
     @BeforeEach
     void setUp() {
-        Configuration.headless = true;
+        //Configuration.headless = true;
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1800x1100";
         Configuration.timeout = 15000;
         open("http://localhost:9999/");
     }
+
     public String generateDate(long addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
     void shouldRegisterByAccountNumberDOMModification() {
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").doubleClick();
-        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        String planningDate = generateDate(3, "dd.MM.yyyy");
-        $("[placeholder='Дата встречи']").setValue(planningDate);
+        $("[placeholder='Город']").setValue("Ка");
+        $(byText("Калининград")).click();
+        $(".icon_name_calendar").click();;
+        $x("[//div//td[contains(text(), '22')]").click();
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+71234567890");
         $("[data-test-id='agreement']").click();
